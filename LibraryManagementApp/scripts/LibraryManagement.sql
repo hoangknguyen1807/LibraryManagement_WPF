72c4a2fa6c2e465ac9dc34fb4a9727109a39e215
hoangknguyen1807 <hoangknguyen.1807@gmail.com>
2020-04-23 01:15:13 +0700

    Added SQL Server database script

---
+++ LibraryManagementApp/scripts/LibraryManagement.sql
@@ -0,0 +1,77 @@
+﻿CREATE DATABASE Library
+--DROP DATABASE Library
+GO
+
+USE [Library]
+GO
+
+CREATE TABLE [dbo].[Books](
+	[ID] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Category] [nvarchar](50) NULL,
+	[Author] [nvarchar](50) NULL,
+	[Publisher] [nvarchar](50) NULL,
+	[YearPublished] [int] NULL,
+	[Price] [int] NULL,
+	[ISBN] [nvarchar](20) NULL,
+	PRIMARY KEY (ID)
+) 
+GO
+
+INSERT Books (ID, Name, Category, Author, Publisher, YearPublished, Price) VALUES
+(1, N'Sherlock Holmes Tập 1', N'Trinh thám', 'Conan Doyle', N'Hồng Đức', 2014, 75000)
+INSERT Books (ID, Name, Category, Author, Publisher, YearPublished, Price) VALUES
+(2, N'Sherlock Holmes Tập 2', N'Trinh thám', 'Conan Doyle', N'Hồng Đức', 2014, 75000)
+INSERT Books (ID, Name, Category, Author, Publisher, YearPublished, Price) VALUES
+(3, N'Sherlock Holmes Tập 3', N'Trinh thám', 'Conan Doyle', N'Hồng Đức', 2014, 75000)
+INSERT Books (ID, Name, Category, Author, Publisher, YearPublished, Price) VALUES
+(4, N'Lập trình Hướng đối tượng', N'Khoa học Máy tính', N'Trần Đan Thư', N'KHTN', 2015, 80000)
+INSERT Books (ID, Name, Category, Author, Publisher, YearPublished, Price) VALUES
+(5, N'Tôi Tự Học', N'Học tập', N'Nguyễn Duy Cần', N'Trẻ', 2016, 60000)
+GO
+
+-- Stored Procedures
+IF OBJECT_ID ('SP_GetAllBooks')
+IS NOT NULL
+DROP PROCEDURE SP_GetAllBooks
+GO
+CREATE PROCEDURE SP_GetAllBooks
+AS
+	SELECT * FROM Books
+GO
+
+--EXEC SP_GetAllBooks
+
+IF OBJECT_ID ('SP_AddNewBook')
+IS NOT NULL
+DROP PROCEDURE SP_AddNewBook
+GO
+CREATE PROCEDURE SP_AddNewBook @ID int, @Name nvarchar(50), @Category nvarchar(50),
+@Author nvarchar(50), @Publisher nvarchar(50), @YearPublished int, @Price int,
+@ISBN nvarchar(15)
+AS
+	DECLARE @ret bit
+	IF @ID IN (SELECT ID FROM BOOKS)
+	BEGIN
+		raiserror('New Book Object has Duplicated ID', 16, 1)
+		SET @ret = 0
+		RETURN @ret
+	END
+	INSERT INTO BOOKS VALUES
+	(@ID, @Name, @Category, @Author, @Publisher, @YearPublished, @Price, @ISBN)
+	SET @ret = 1
+	RETURN @ret
+GO
+
+--EXEC SP_AddNewBook 6, N'Tony Buổi tối', null, null, null, null, null, '19827498'
+
+IF OBJECT_ID ('SP_DeleteBook')
+IS NOT NULL 
+DROP PROCEDURE SP_DeleteBook
+GO
+CREATE PROCEDURE SP_DeleteBook @bID int
+AS
+	DELETE FROM BOOKS WHERE ID = @bID
+GO
+
+--EXEC SP_DeleteBook 6
